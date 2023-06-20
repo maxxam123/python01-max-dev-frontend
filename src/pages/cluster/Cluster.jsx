d1c08883b50e055453d7a78eb9bfe7466071b99a
max <81824845+maxxam123@users.noreply.github.com>
2023-06-20 09:26:41 +0200

    Create Cluster.jsx

---
+++ src/pages/cluster/Cluster.jsx
@@ -0,0 +1,16 @@
+import "./cluster.css"
+import Feed from "../../components/feed/Feed"
+import { Link } from 'react-router-dom';
+
+export default function Cluster() {
+  return (
+    <div>
+    <div className="cluster">
+      <Link className="clusterlink" to='/rea'>Go to Reactpage</Link>
+      <Link className="clusterlink" to='/home'>Go to Homepage</Link>
+      Create Cluster
+      <Feed/>
+      </div>
+    </div>
+  )
+}
